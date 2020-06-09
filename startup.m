3f3fa2bd9ecde3b3d6a6fa68a4c10658ebbaafe3
James <james.haley@ucdenver.edu>
2020-06-09 12:48:00 -0600

    adding input for windows or linux to startup.m

--- startup.m
+++ startup.m
@@ -1,10 +1,15 @@
-function msg = startup()
+function msg = startup(os)
 % adds daily directories to the path
-
+% input: 
+%     os - integer 0=windows 1=linux
 d = dir('*_*');
 for i = 1:length(d)
     if d(i).isdir
-       s = [d(i).folder,'/',d(i).name];
-       addpath(s)
+        if os == 0
+            s = [pwd,'\',d(i).name];
+        else
+            s = [d(i).folder,'/',d(i).name];
+        end
+        addpath(s);
     end
 end
